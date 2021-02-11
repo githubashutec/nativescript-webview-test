2c048c94465a1b861006c8762b6bb48d7f9f9ce1
Milan Saraiya <milan@ashutec.com>
2021-02-11 15:24:13 +0530

    changes

--- src/app/webview/webview.component.ts
+++ src/app/webview/webview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { WebViewExt } from "@nota/nativescript-webview-ext";
 
 @Component({
     selector: "ns-webview",
@@ -13,12 +14,22 @@ export class WebviewComponent implements OnInit {
     }
     // used nota:WebViewExt webview extension and used executeJavaScript function to inject javascript which will remove _blank attribute from links 
     loadFinished(event) {
-        const webview = event.object;
-        setTimeout(async () => {
-            await webview.executeJavaScript(`links = document.links;
-            for (i = 0, length = links.length; i < length; i++) {
-                 links[i].target == '_blank' && links[i].removeAttribute('target');
-             }`);
-        }, 1000);
+        const webview = event.object as WebViewExt;
+        //webview.registerLocalResource('test.xjs', '~assets/js/test.xjs');
+        // if(webview.src.includes("influencer-app.lifo.ai") || webview.src.includes("influencer-beta.lifo.ai")) {
+        //     webview.autoLoadJavaScriptFile('test.xjs', '~/assets/js/test.xjs');
+        // }
+        webview.autoLoadJavaScriptFile('test.xjs', '~/assets/js/test.xjs');
+        webview.on('openInExternalBrowser', (msg) => console.log('need to open in external browser',msg.data));
+        // webview.autoExecuteJavaScript(`
+        // helloWorld();
+        // `
+        // ,'helloWorld');
+        // setTimeout(async () => {
+        //     await webview.executeJavaScript(`links = document.links;
+        //     for (i = 0, length = links.length; i < length; i++) {
+        //          links[i].target == '_blank' && links[i].removeAttribute('target');
+        //      }`);
+        // }, 1000);
     }
 }
